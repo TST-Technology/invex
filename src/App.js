005c55dc2fc086035c39db919117b0f50f15d879
Nishant <nishant@eldmandate.co>
2021-12-07 20:14:12 +0530

    added scroll to top on page change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { Routes, Route, HashRouter, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 import "./style.css";
-import Navbar from './component/Common/Navbar/Navbar';
-import Home from './component/Home/Home';
+import Navbar from "./component/Common/Navbar/Navbar";
+import Home from "./component/Home/Home";
 import Screener from "./component/Screener/Screener";
-import Footer from './component/Common/Footer/Footer';
-import Market from './component/Market/Market';
-import Sectors from './component/Common/Sectors/Sectors';
-import News from './component/News/News';
-import Options from './component/Options/Options';
-import EconomicData from './component/EconomicData/EconomicData';
-import Dashboard from './component/Dashboard/Dashboard';
-import SymbolPage from './component/Symbol/SymbolPage';
-import VideoResource from './component/VideoResource/VideoResource';
-import LogIn from './component/Authentication/LogIn/LogIn';
+import Footer from "./component/Common/Footer/Footer";
+import Market from "./component/Market/Market";
+import Sectors from "./component/Common/Sectors/Sectors";
+import News from "./component/News/News";
+import Options from "./component/Options/Options";
+import EconomicData from "./component/EconomicData/EconomicData";
+import Dashboard from "./component/Dashboard/Dashboard";
+import SymbolPage from "./component/Symbol/SymbolPage";
+import VideoResource from "./component/VideoResource/VideoResource";
+import LogIn from "./component/Authentication/LogIn/LogIn";
+import GoToTop from "./ScrollToTop";
 
 function App() {
-  
   // useEffect(() => {
   //   const script = document.createElement("script");
   //   script.src = scriptCode;
@@ -31,18 +31,19 @@ function App() {
       <HashRouter>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path="/logIn" element={<LogIn />} />
-          <Route path="/screener" element={<Screener/>} />
-          <Route path="/market" element={<Market />} />
-          <Route path="/sectors" element={<Sectors />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/options" element={<Options />} />
-          <Route path="/economic-data" element={<EconomicData />} />
-          <Route path="/resources" element={<VideoResource />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/symbol" element={<SymbolPage />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/logIn" element={<LogIn />} />
+          <Route exact path="/screener" element={<Screener />} />
+          <Route exact path="/market" element={<Market />} />
+          <Route exact path="/sectors" element={<Sectors />} />
+          <Route exact path="/news" element={<News />} />
+          <Route exact path="/options" element={<Options />} />
+          <Route exact path="/economic-data" element={<EconomicData />} />
+          <Route exact path="/resources" element={<VideoResource />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/symbol" element={<SymbolPage />} />
         </Routes>
+        <GoToTop />
         <Footer />
       </HashRouter>
     </div>
