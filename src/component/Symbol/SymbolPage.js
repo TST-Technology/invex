309d7f3699e82cd879495e7bafc9725aadcdc53d
YogiDhingani <dhinganiyogi120720@gmail.com>
2022-06-18 12:23:06 +0530

    fix: votality chart issue

--- src/component/Symbol/SymbolPage.js
+++ src/component/Symbol/SymbolPage.js
@@ -41,10 +41,10 @@ const SymbolPage = (props) => {
           setKeyStatus(res?.data?.quote);
         }
 
-        // var volatility = await getVolatality(symbol, date);
-        // if (volatility) {
-        //   setOptions(volatility);
-        // }
+        var volatility = await getVolatality(symbol, date);
+        if (volatility) {
+          setOptions(volatility);
+        }
 
         const logoData = await getCompanyLogo(symbol);
         if (
